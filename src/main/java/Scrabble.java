4a228fa7bc3fd9dead142edc076f33ced2020ba4
likewater <lakebayomackway@gmail.com>
2017-03-16 01:16:47 -0700

    add file set with first test.

---
+++ src/main/java/Scrabble.java
@@ -0,0 +1,22 @@
+// import java.util.ArrayList;
+// import java.util.List;
+// import java.util.Array;
+
+public class Scrabble {
+  //int result = 0;
+
+  public Integer calculateScore(String input){
+    Integer score = 0;
+    if (input.equals("a")) {
+      score = 1;
+    }
+    // String[] splitString = word.split("");
+    //   for (Integer i = 0; i < splitString.length; i++)
+    //     if (splitString.indexOf(i) == 'a') {
+    //       score = 1;
+    //     }else{
+    //       score = 0;
+    //     }
+    return score;
+    }
+  }
